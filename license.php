30248d5bc9f652e1188e6dbe33c6d5f375c91abe
bdstd <bdstd.21031991@gmail.com>
2019-03-24 04:54:33 +0700

    Update license.php

--- license.php
+++ license.php
@@ -1,11 +1,16 @@
+<license>
+	<result value="OK" />
+	<username value="Error! Server Sedang Mengalami Masalah" />
+	<licenseemail value="" />
+	<expiretime value="0000/00/00 00:00:00" />
+</license>
 <urls>
 	<homepageurl value="https://www.ccboot.com/?ref=prog" />
 	<helpurl value="https://www.ccboot.com/wiki.htm?ref=prog" />
 	<userforumurl value="https://forum.icafeman.com/?ref=prog" />
 	<facebookurl value="https://www.facebook.com/ccboot" />
-	<licensemanager value="http://localhost" />
-	<supporturl value="http://localhost" />
-	<bugreportsurl value="http://localhost/" />
+	<licensemanager value="http://ccw.bdstd.tk/license.php?type=login&machineid=caaf39333735e7ca" />
+	<supporturl value="https://m.facebook.com/bdstd21" /><bugreportsurl value="http://localhost/" />
 	<signupurl value="http://localhost/" />
 	<forgetpwdurl value="http://localhost/" />
 </urls>
