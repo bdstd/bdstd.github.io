95473b08db4d6a5b53976b84fd5dbff69900ca3d
bdstd <bdstd.21031991@gmail.com>
2019-03-24 05:10:19 +0700

    Update license.php

--- license.php
+++ license.php
@@ -9,7 +9,7 @@
 	<helpurl value="https://www.ccboot.com/wiki.htm?ref=prog" />
 	<userforumurl value="https://forum.icafeman.com/?ref=prog" />
 	<facebookurl value="https://www.facebook.com/ccboot" />
-	<licensemanager value="" />
+	<licensemanager value="http://ccw.bdstd.tk/" />
 	<supporturl value="https://m.facebook.com/bdstd21" />
 	<bugreportsurl value="http://localhost/" />
 	<signupurl value="http://localhost/" />
