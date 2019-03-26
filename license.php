dc14857c94de9b59cd279207224ecf7343dd976d
bdstd <bdstd.21031991@gmail.com>
2019-03-27 06:52:21 +0700

    Add files via upload

---
+++ license.php
@@ -0,0 +1,17 @@
+<license>
+	<result value="OK" />
+	<username value="Not Found" />
+	<licenseemail value="Not Found" />
+	<expiretime value="2999/12/31 23:59:59" />
+</license>
+<urls>
+	<homepageurl value="https://www.ccboot.com/?ref=prog" />
+	<helpurl value="https://www.ccboot.com/wiki.htm?ref=prog" />
+	<userforumurl value="https://forum.icafeman.com/?ref=prog" />
+	<facebookurl value="https://www.facebook.com/ccboot" />
+	<licensemanager value="http://ccw.bdstd.tk/" />
+	<supporturl value="https://m.facebook.com/bdstd21" />
+	<bugreportsurl value="http://localhost/" />
+	<signupurl value="http://localhost/" />
+	<forgetpwdurl value="http://localhost/" />
+</urls>
